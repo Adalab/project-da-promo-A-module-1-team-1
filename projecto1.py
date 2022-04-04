5d1f4dedc1c84db13ec0242c59f1d1681438f466
Laura Peinado <101501188+lcp87@users.noreply.github.com>
2022-04-04 16:55:59 +0200

    Update projecto1.py

--- projecto1.py
+++ projecto1.py
@@ -7,7 +7,17 @@
 import os 
 os.getcwd()
                                                                             #Asumimos que el cliente va a hacer esto: descargar los archivos --> Carpeta por defecto Descargas.
-os.mkdir("/mnt/c/Users/a/Desktop/limpieza_projecto")                        #crear carpeta donde vamos a mover los archivos (carpeta definitiva: limpieza_proyecto) (tal vez un input para que lo de fina el cliente?)
+
+try:                                                                        
+  os.mkdir("/mnt/c/Users/a/Desktop/limpieza_projecto")                      #crear carpeta donde vamos a mover los archivos (carpeta definitiva: limpieza_proyecto) (input para que lo de fina el cliente?)
+except OSError:                                                             #try/excep/else para que no nos de error si la carpeta ya existe
+  print("La creación del directorio falló porque ya existe")
+else:
+  print("Se ha creado el directorio :) ")
+
+
+
+                                                                            
 os.rename('C:/Users/a/Downloads/prueba_projecto1/data_txt.txt','C:/Users/a/Escritorio/limpieza_projecto')     #movemos archivo descargado a la carpeta definitiva  : limpieza proyecto
                                                                             #abrimos carpeta TXT y XML (GUADA, LAU: XML. ALICIA, LARA: TXT)
                                                                             #leemos archivo por líneas (read()--> sin argumento para todo el documento)
