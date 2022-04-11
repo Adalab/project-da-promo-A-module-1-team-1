735dc4f57a99b665e0abc44338e01cceb81db741
Laura Peinado <101501188+lcp87@users.noreply.github.com>
2022-04-11 16:14:41 +0200

    Update parte_xml.py

--- parte_xml.py
+++ parte_xml.py
@@ -1,7 +1,11 @@
 import os
 import xml.etree.ElementTree as ET
 import re
+import shutil
 
+
+os.getcwd()
+os.chdir('/mnt/c/Users/a/Desktop/Adalab/ejercicios-resueltos-clase-invertida-DA-promoA')
 
 origen = './data_xml.xml'
 destino = "./limpieza_projecto"
@@ -33,12 +37,10 @@ with open('data_xml.xml', 'r') as f:
 #PROBANDO A BUSCAR EL PATRÓN DE LA FILA A ELIMINAR EN EL XML
 
 patron_eliminar_xml = '''<level_0>25969</level_0> 
-
-
 # CAMBIAR el elemento género en el xml?
 <index>25969</index> 
 <time>253</time>'''
-na_probando = re.findall('\<level\_0\>\[\d]<\/level\_0\>',string_xml)
+na_probando = re.findall('\<level\_0\>\[\d]<\/level\_0\>',patron_eliminar_xml)
 
 print(na_probando)
 
@@ -46,7 +48,7 @@ print(na_probando)
 ## FOR para buscar e ese patrón en todo el documento xlm
 # import xml.etree.ElementTree as ET
 
-tree = ET.parse('contacto.xml')
+tree = ET.parse('data_xml_trozo.xml')
 root = tree.getroot()
 print(root.tag)
 print(root.attrib)
@@ -54,4 +56,7 @@ for child in root:
     print(child.tag)
     for subchild in child:
         print('  ',subchild.tag)
+        subchild = subchild.tag
 
+# for level in root.findall('level_0'):
+#     level.remove()
