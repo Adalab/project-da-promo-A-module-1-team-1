e3c43f45ed78bff4814fd3fc67c91a058e35a063
Laura Peinado <101501188+lcp87@users.noreply.github.com>
2022-04-11 17:31:32 +0200

    Update parte_sql.py

--- parte_sql.py
+++ parte_sql.py
@@ -1,1 +1,25 @@
+#ABRIR el archivo sql en Python
 
+
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='AlumnaAdalab',
+                              host='127.0.0.1', database='BD_pruebas')
+
+
+mycursor = cnx.cursor()
+sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'"
+try:
+    mycursor.execute(sql)
+    cnx.commit()
+    print(mycursor.rowcount, "registro/s modificado/s.")
+
+except mysql.connector.Error as err:
+    print(err)
+    print("Error Code:", err.errno)
+    print("SQLSTATE", err.sqlstate)
+    print("Message", err.msg)
+
+mycursor.execute("SELECT * FROM customers")  
+results = mycursor.fetchall()
+print(results)
