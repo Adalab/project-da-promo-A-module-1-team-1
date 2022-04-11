ff4993839593c7d1beac807135abc68061455ed6
Laura Peinado <101501188+lcp87@users.noreply.github.com>
2022-04-11 13:34:39 +0200

    Update projecto1.py

--- projecto1.py
+++ projecto1.py
@@ -35,6 +35,32 @@ os.rename('C:/Users/a/Downloads/prueba_projecto1/data_txt.txt','C:/Users/a/Escri
 
 
 
+###LISTA DE ERRORES
+#ESTE ES EL ESQUEMA DE LOS POSIBLES ERRORES A LIMPIAR HECHO EN GITHUB.
+#A TENERLO EN CUENTA MÁS ADELANTE.
+# Después de observar los archivos hemos llegado a la conclusión que los posibles errores de limpieza a solucionar son:
+# Esperando votación SOLO EN XML de todas las integrantes. Las opciones para TXT se van a realizar TODAS. Propuestas de día 01/04.
+
+# En: XML:
+
+#  Limpieza la parte de la fila donde pone level es [0] porque siempre es igual (elegir esta): GUADA.
+
+#  Limpieza de la fila total ya que tiene la misma info que index. En el caso que se necesite la fila level, borramos solo la parte del 0 (o elegir esta). LAURA, ALICI, LARA.
+
+# En: TXT:
+
+#  Asumiendo que NO conocemos las unidades de la variable tiempo (el cliente no nos la ha dicho), podemos dejar sin hacer una conversión de momento.
+
+#  Cadena de Nulls no se repiten en otras las filas, así que no podemos eliminarlas.
+
+#  Limpiar espacios sobrantes de:
+# - antes de la coma (pueden ser 1 o 2 espacios demás) ( ej: ;null, Visual Studio Code (VSCode) ,
+# PyCharm , Jupyter Notebook;
+# - antes y después palabra (ejemplo: PyCharm )
+
+
+
+
 
 
 
