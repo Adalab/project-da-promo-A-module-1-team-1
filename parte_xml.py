26319c0083b3d4a56e5723d335adc60bc38f572c
Laura Peinado <101501188+lcp87@users.noreply.github.com>
2022-04-12 18:08:22 +0200

    Update parte_xml.py

--- parte_xml.py
+++ parte_xml.py
@@ -1,65 +1,58 @@
 import os
 import xml.etree.ElementTree as ET
 import re
-import shutil
 
-
-os.getcwd()
-os.chdir('/mnt/c/Users/a/Desktop/Adalab/ejercicios-resueltos-clase-invertida-DA-promoA')
-
-origen = './data_xml.xml'
+ 
+os.chdir("/mnt/c/Users/a/Desktop/Adalab/ejercicios-resueltos-clase-invertida-DA-promoA/")
+print("Estamos aqui", os.getcwd())
+origen = '/mnt/c/Users/a/Desktop/Adalab/ejercicios-resueltos-clase-invertida-DA-promoA/'
 destino = "./limpieza_projecto"
 archivo = os.path.basename(origen)
-print(os.getcwd())
 
-tree = ET.parse('data_xml.xml')
+
+############################################################################## OPCIÓN 1: compleja
+tree = ET.parse('data_xml_trozo.xml')
 root = tree.getroot()
-print(root)
+print("Esta es la ruta, ", root)
+
+# # data_nuevo = ET.parse('data_xml_limpio.xml')
+with open('data_xml_trozo.xml', 'r') as data:
+    
+
+    for child in root:
+        print(child.tag)
+        
+        for subchild in child:
+            linea_level = re.findall('\<level\_0\>[\d]*', data.read())
+            linea_nueva = re.sub('\<level\_0\>[\d]*'," ", data.read())
+            print('  ',subchild.tag)
+ 
+with open('data_xml_limpio.xml', 'w') as data:
+    xml_nuevo = data.write(linea_nueva)
+    print(xml_nuevo)
+
+########################################################################## OPCIÓN 2: simple
+
+with open('data_xml_trozo.xml', 'r') as data:
+    data = root.read()
+
+    for line in data:
+            if line.tag == "level_0":
+                line.tag.remove()
+            else:
+                pass
+        
+    with open('data_xml_limpio.xml', 'w') as data_nuevo:  
+        
+        
 
 
-with open('data_xml.xml', 'r') as f:
-    data = f.read()
 
-# print(data)                                 # comprobación de que abre el archivo xml
-
-
-
-# PROBANDO CÓMO ELIMINAR FILA
-
-# root.remove(b)
-# ElementTree.dump(root)
-# OUTPUT
-# <root>
-#    <a>1</a>
-#    </root>
-
-
-#PROBANDO A BUSCAR EL PATRÓN DE LA FILA A ELIMINAR EN EL XML
-
-patron_eliminar_xml = '''<level_0>25969</level_0> 
-# CAMBIAR el elemento género en el xml?
-<index>25969</index> 
-<time>253</time>'''
-na_probando = re.findall('\<level\_0\>\[\d]<\/level\_0\>',patron_eliminar_xml)
-
+        
+        
+#PROBANDO a eliminar patrón
 print(na_probando)
 #elminacion=doc.find("xml")
 #eliminacion.remove(eliminacion.find("'\<level\_0\>\[\d]<\/level\_0\>")
 #La intencion es crear un bucle donde encuentre la palabra level por cada root y la elimine
 
-
-## FOR para buscar e ese patrón en todo el documento xlm
-# import xml.etree.ElementTree as ET
-
-tree = ET.parse('data_xml_trozo.xml')
-root = tree.getroot()
-print(root.tag)
-print(root.attrib)
-for child in root:
-    print(child.tag)
-    for subchild in child:
-        print('  ',subchild.tag)
-        subchild = subchild.tag
-
-# for level in root.findall('level_0'):
-#     level.remove()
